04c7abfa5bafb49dc1fa5bc604f769b2a9959a70
Lucas Daniel Veríssimo <lucasdaniel.verissimo@gmail.com>
2019-11-12 07:56:43 -0300

    Inicio das consultas SQL

---
+++ Consultas/consultas.sql
@@ -0,0 +1,9 @@
+-- Inserir cliente como pessoa física
+INSERT INTO Cliente(nome, endereco, sexo, data_nasc, cpf) VALUES
+('Joao Silva', 'Rua Sete 820', 'M', 1999-5-18, 11122233344);
+
+-- Inserir cliente como pessoa jurídica
+INSERT INTO Cliente(nome, endereco, cnpj, inscr_estado) VALUES
+('Joao Silva', 'Rua Sete 820', 18853410000140, 0223233556);
+
+-- 
